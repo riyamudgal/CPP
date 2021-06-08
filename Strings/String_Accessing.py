9509212a725a6292eabdcc7de258c0fb04050ce1
Riya Mudgal <75576621+riyamudgal@users.noreply.github.com>
2021-06-09 00:22:37 +0530

    Create String_Accessing.py

---
+++ Strings/String_Accessing.py
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+  //accessing string Characters
+  string myString = "Hello";
+  cout << myString[0];
+  
+  //Changing string character
+  string str = "Week";
+  str[2] = 'a';
+  cout << str;
+  
+ //string concatenation 
+ string firstName = "John ";
+string lastName = "Doe";
+string fullName = firstName + lastName;
+cout << fullName;
+  
+//
+ string x = "10";
+int y = "20";
+string z = x + y;
+  cout<<z;
+  
+ 
+  
+  
+  
+  return 0;
+}
