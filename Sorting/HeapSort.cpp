45844952f85b04b9f20d7ec3ea8a06c0883c6d04
Riya Mudgal <75576621+riyamudgal@users.noreply.github.com>
2021-06-23 00:13:14 +0530

    Create HeapSort.cpp

---
+++ Sorting/HeapSort.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+
+
+void heapify(int arr[], int n, int i)
+{
+    int largest = i; 
+    int l = 2*i+1; 
+    int r = 2*i; 
+ 
+    if (l<n && arr[l]>arr[largest])
+        largest = l;
+ 
+    if (r<n && arr[r]>arr[largest])
+        largest = r;
+ 
+    if (largest!=i) 
+    {
+        swap(arr[i],arr[largest]);
+        heapify(arr,n,largest);
+    }
+}
+void heapSort(int arr[], int n)
+{
+    for (int i = n / 2 - 1; i >= 0; i--)
+        heapify(arr, n, i);
+ 
+    for (int i = n - 1; i > 0; i--)
+     {
+        swap(arr[0], arr[i]);
+        heapify(arr, i, 0);
+    }
+}
+void display(int arr[], int n)
+{
+    for (int i = 0; i < n; ++i)
+        cout<<arr[i]<<" ";
+    cout << "\n";
+}
+int main()
+{
+    int arr[]={23, 11, 8, 1, 5, 15, 19};
+    int n = arr.size();
+    heapSort(arr, n);
+ 
+    cout << "Sorted array is-> "<<endl;
+    display(arr, n);
+}
